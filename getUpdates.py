73435c84a0f0b67bd2b55fa4a36421bc7127a9ab
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-06-23 12:46:50 +0500

    getUpdates

--- getUpdates.py
+++ getUpdates.py
@@ -1,10 +1,11 @@
 import requests
+from pprint import pprint
 
-TOKEN = ''
+TOKEN = '6089863461:AAGuL8gKw5QdjXaqwcNY6dZmQmezYVgtDUg'
 BASE_URL = f'https://api.telegram.org/bot{TOKEN}/getUpdates'
 
 
 response = requests.get(url=BASE_URL)
 updates=response.json()['result']
-
+pprint(response.json())
 # print each update
