d78f97855b90fd338e25d9a88f27112be7d6b304
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-06-23 12:45:45 +0500

    send message

--- sendMessage.py
+++ sendMessage.py
@@ -1,9 +1,17 @@
 import os
 import requests
 
-TOKEN = ''
+TOKEN = '6089863461:AAGuL8gKw5QdjXaqwcNY6dZmQmezYVgtDUg'
 
 def sendMessage(chat_id:str, text:str):
     URL = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
+    params={
+        'chat_id':chat_id,
+        'text':text
+    }
+    r=requests.get(url=URL,params=params)
     
-    
+    return r.json()
+
+print(sendMessage(chat_id='1937466761',text='salom'))
+sendMessage(chat_id='1937466761',text='salom')
